5061d8d0e54f763fa02859d29ade57ef664663d4
myjustify <1406023741@qq.com>
2021-08-20 22:39:34 +0800

    云函数

--- template/common/vue.config.js
+++ template/common/vue.config.js
@@ -21,7 +21,7 @@ module.exports = {
 		plugins: [
 			new CopyWebpackPlugin([
 				{
-					from: path.join(__dirname, './cloudfunctions'),
+					from: path.join(__dirname, './src/cloudfunctions'),
 					to: path.join(__dirname, 'dist', process.env.NODE_ENV === 'production' ? 'build' : 'dev', process.env.UNI_PLATFORM, 'cloudfunctions'),
 				},
 			]),
